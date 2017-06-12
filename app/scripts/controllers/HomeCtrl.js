51ff9ff42905ef76b00eb501c71bb94baa1484f9
perryahern <pahern@gmail.com>
2017-06-12 18:27:47 -0400

    Bloc Chat checkpoint 4 working pre-cleanup

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,10 +1,21 @@
 (function() {
-    function HomeCtrl(Room) {
+    function HomeCtrl(Room, Message, $scope) {
         this.rooms = Room.all;
-        this.currentRoom = 'test';
+        $scope.currentRoom = 'test';
+        this.currentRoom = null;
+
+        this.changeRoom = function(roomKey) {
+            console.log("in changeRoom");
+            console.log("roomKey received: " + roomKey);
+            console.log("previous value: " + $scope.currentRoom);
+            $scope.currentRoom = roomKey;
+            console.log("after assignment: " + $scope.currentRoom);
+            this.currentRoom = roomKey;
+            this.messages = Message.getByRoomId(this.currentRoom);
+        };
     }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', 'Message', '$scope', HomeCtrl]);
 })();
--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -3,8 +3,13 @@
     var Message = {};
     var ref = firebase.database().ref().child("messages");
     var messages = $firebaseArray(ref);
-    
-Message.all = $firebaseArray(ref);
+
+    Message.getByRoomId = function(newRoomId) {
+        return $firebaseArray(ref.orderByChild('roomId').equalTo(newRoomId));
+    };
+
+
+    Message.all = $firebaseArray(ref);
     return Message;
   }
 
